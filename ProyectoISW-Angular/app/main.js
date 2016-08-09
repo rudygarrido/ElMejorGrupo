f21e957886ffacc4e47fcf3f191aa1d7cf0517ef
Daes <running_rinception@hotmail.com>
2016-08-09 14:24:52 -0600

    Agregando folder de Angular

---
+++ ProyectoISW-Angular/app/main.js
@@ -0,0 +1,7 @@
+
+
+(function(app) {
+  document.addEventListener('DOMContentLoaded', function() {
+    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
+  });
+})(window.app || (window.app = {}));
